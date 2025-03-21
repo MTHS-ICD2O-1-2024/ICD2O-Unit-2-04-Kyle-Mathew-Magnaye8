e66c01625ad60bab580e241dbb9db721709fe5d7
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-03-21 16:54:49 +0000

    save changes php 2

--- js/script.js
+++ js/script.js
@@ -4,9 +4,27 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates area of a triangle.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculatePermeterAndAreaOfRectangle() {
+  // input
+  const lengthOfRectangle = parseInt(
+    document.getElementById("length-of-rectangle").value
+  )
+  const heightOfRectangle = parseInt(
+    document.getElementById("width-of-rectangle").value
+  )
+
+  // process
+  const areaOfRectangle = lengthOfRectangle * heightOfRectangle
+  const perimeterOfRectangle = (lengthOfRectangle + heightOfRectangle) * 2
+
+  // output
+  document.getElementById("area").innerHTML =
+    "Area is: " + areaOfRectangle + " cm²"
+  document.getElementById("perimeter").innerHTML =
+    "Perimeter is: " + perimeterOfRectangle + " cm"
 }
